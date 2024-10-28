16461abfbaf0c07313461bc2efd61f9ca4be388d
Noura-H-Mahmoud <nouramahmoud@Nouras-MacBook-Air.local>
2024-10-28 19:52:43 +0400

    the last commit

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,7 +3,7 @@ export default function Input({ value, placeholder }) {
     return (
         <div className=' w-[494px] max-md:w-full'>
             <div className="w-full flex gap-4 max-md:flex-col justify-center">
-                <input type="text" placeholder={placeholder} className="border-[1px] border-[#D0D5DD] rounded-lg h-12 w-[360px] max-md:w-full pt-3 pb-3 pl-4 pr-3.5 outline-[#7F56D9] shadow-none" />
+                <input type="text" placeholder={placeholder} className="border-[1px] border-[#D0D5DD] rounded-lg h-12 w-[360px] max-md:w-full pt-3 pb-3 pl-4 pr-3.5 outline-[#7F56D9] focus:shadow-none" />
                 <input type="submit" value={value} className="w-[118px] h-12 bg-[#7F56D9] 
                 text-white rounded-lg hover:border hover:border-solid hover:border-[#7F56D9] 
                 hover:bg-white hover:text-[#7F56D9] transition-colors duration-300" />
